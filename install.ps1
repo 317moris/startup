6dc214f77ccdaa90904c7fd5edad18d93ef814e5
317moris <mori.souta.fu@st.spec.ed.jp>
2025-09-17 11:39:40 +0900

    a

--- install.ps1
+++ install.ps1
@@ -1,1 +1,5 @@
-$packages = ""
+$packages = "Microsoft.PowerShell", "Git.Git", "Mozilla.Firefox.ja", "Volta.Volta", "MacType.MacType", "Starship.Starship", "Google.JapaneseIME"
+
+for ($i = 0; $i -lt $packages.Count; $i++) {
+    & winget install $packages[$i]
+}
