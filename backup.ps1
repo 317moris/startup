d94b7700d45f2a942b9413c3c0bbdddc59ea8a7e
317moris <mori.souta.fu@st.spec.ed.jp>
2025-11-12 11:29:14 +0900

    update

--- backup.ps1
+++ backup.ps1
@@ -1,10 +1,11 @@
 $networkDriveLetter = "Z"
-$networkAppData = "${networkDriveLetter}:\System\AppData\Roaming"
+$networkAppData = "${networkDriveLetter}:\AppData\Roaming"
 
-Remove-Item -Path "${networkAppData}\Mozilla\*" -Force
+Remove-Item -Path "${networkAppData}\*" -Force -Recurse
 Remove-Item -Path "${networkDriveLetter}:\Microsoft.PowerShell_profile.ps1"
-Remove-Item -Path "${networkDriveLetter}:\.vscode\*" -Force
+Remove-Item -Path "${networkDriveLetter}:\.vscode\*" -Force -Recurse
 
-Copy-Item -Path "${env:APPDATA}\Mozilla\*" -Destination "${networkAppData}\Mozilla" -Recurse
-Copy-Item -Path $PROFILE -Destination "${networkDriveLetter}:\Microsoft.PowerShell_profile.ps1"
-Copy-Item -Path "${env:USERPROFILE}\.vscode\*" -Destination "${networkDriveLetter}:\.vscode"
+Copy-Item -Path "${env:APPDATA}\Mozilla\" -Destination "${networkAppData}\" -Recurse
+Copy-Item -Path $PROFILE -Destination "${networkDriveLetter}:\"
+Copy-Item -Path "${env:USERPROFILE}\.vscode\" -Destination "${networkDriveLetter}:\" -Recurse
+Copy-Item -Path "${env:APPDATA}\Code\User\" -Destination "${networkAppData}\Code\" -Recurse
--- install.ps1
+++ install.ps1
@@ -1,16 +1,21 @@
-$packages = "MacType.MacType", "Mozilla.Firefox.ja", "Microsoft.PowerShell", "Git.Git", "Volta.Volta", "Starship.Starship", "Google.JapaneseIME"
+$packages = "MacType.MacType", "Mozilla.Firefox.ja", "Microsoft.PowerShell", "Git.Git", "nodejs", "Starship.Starship", "Google.JapaneseIME"
 $networkDriveLetter = "Z"
-$networkAppData = "${networkDriveLetter}:\System\AppData\Roaming"
+$devDriveLetter = "E"
+$networkAppData = "${networkDriveLetter}:\AppData\Roaming"
 
-Copy-Item -Path "${networkAppData}\Mozilla\*" -Destination "${env:APPDATA}\Mozilla" -Recurse
-Copy-Item -Path "${networkDriveLetter}:\Microsoft.PowerShell_profile.ps1" -Destination "${env:USERPROFILE}\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
-Remove-Item -Path "${env:USERPROFILE}\.vscode\*" -Force
-Copy-Item -Path "${networkDriveLetter}:\.vscode\*" -Destination "${env:USERPROFILE}\.vscode"
+New-Item -Path "${env:USERPROFILE}\Documents\" -Name "PowerShell" -ItemType "Directory" 
+New-Item -Path "${env:USERPROFILE}\" -Name ".config" -ItemType "Directory"
+
+Copy-Item -Path "${networkAppData}\Mozilla\" -Destination "${env:APPDATA}\" -Recurse
+Copy-Item -Path "${networkDriveLetter}:\Microsoft.PowerShell_profile.ps1" -Destination "${env:USERPROFILE}\Documents\"
+Remove-Item -Path "${env:USERPROFILE}\.vscode\" -Force
+Copy-Item -Path "${networkDriveLetter}:\.vscode\" -Destination "${env:USERPROFILE}\"
+Copy-Item -Path "${networkAppData}\Code\" -Destination "${env:APPDATA}\" -Recurse
 
 for ($i = 0; $i -lt $packages.Count; $i++) {
     winget install $packages[$i]
 }
 
-Start-Process -FilePath "volta" -ArgumentList "install node@latest"
-Start-Process -FilePath "git" -ArgumentList "clone https://github.com/317moris/ksg-website.git ${env:USERPROFILE}\ksg-website"
+Start-Process -FilePath "C:\Program Files\Git\cmd\git.exe" -ArgumentList "clone https://github.com/317moris/ksg-website.git ${devDriveLetter}:\ksg-website"
 Invoke-RestMethod bun.sh/install.ps1 | Invoke-Expression
+Start-Process -FilePath "C:\Program Files\starship\bin\starship.exe" -ArgumentList "preset bracketed-segments -o ${env:USERPROFILE}\.config\starship.toml"
