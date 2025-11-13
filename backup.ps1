1dd714ff50f124b0ffdef0bb6a0c581c9ab00b1b
317moris <mori.souta.fu@st.spec.ed.jp>
2025-11-13 11:51:19 +0900

    update

--- backup.ps1
+++ backup.ps1
@@ -7,8 +7,8 @@ Remove-Item -Path "${networkDriveLetter}:\Microsoft.PowerShell_profile.ps1"
 Remove-Item -Path "${networkDriveLetter}:\.vscode\" -Force -Recurse
 Remove-Item -Path "${networkLocalAppData}\*" -Recurse -Force
 
-Copy-Item -Path "${env:APPDATA}\Mozilla\" -Destination "${networkAppData}\" -Recurse
+Copy-Item -Path "${env:APPDATA}\Mozilla\" -Destination "${networkAppData}\" -Recurse -Force
 Copy-Item -Path $PROFILE -Destination "${networkDriveLetter}:\"
-Copy-Item -Path "${env:USERPROFILE}\.vscode\" -Destination "${networkDriveLetter}:\" -Recurse
-Copy-Item -Path "${env:APPDATA}\Code\" -Destination "${networkAppData}\" -Recurse
+Copy-Item -Path "${env:USERPROFILE}\.vscode\" -Destination "${networkDriveLetter}:\" -Recurse -Force
+Copy-Item -Path "${env:APPDATA}\Code\" -Destination "${networkAppData}\" -Recurse -Force
 Copy-Item -Path "${env:LOCALAPPDATA}\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\" -Destination "${networkLocalAppData}\Packages\" -Recurse -Force
